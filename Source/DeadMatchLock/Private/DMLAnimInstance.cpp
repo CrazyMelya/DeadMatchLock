cf991500580218f222316092ed90eb1a21346cb0
CrazyMelya <crazymelya@gmail.com>
2025-05-02 01:35:02 +0400

    something

---
+++ Source/DeadMatchLock/Private/DMLAnimInstance.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DMLAnimInstance.h"
+
+#include "AbilitySystemGlobals.h"
+#include "DMLCharacter.h"
+#include "Misc/DataValidation.h"
+
+UDMLAnimInstance::UDMLAnimInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void UDMLAnimInstance::InitializeWithAbilitySystem(UAbilitySystemComponent* ASC)
+{
+	check(ASC);
+
+	GameplayTagPropertyMap.Initialize(this, ASC);
+}
+
+#if WITH_EDITOR
+EDataValidationResult UDMLAnimInstance::IsDataValid(FDataValidationContext& Context) const
+{
+	Super::IsDataValid(Context);
+
+	GameplayTagPropertyMap.IsDataValid(this, Context);
+
+	return ((Context.GetNumErrors() > 0) ? EDataValidationResult::Invalid : EDataValidationResult::Valid);
+}
+#endif // WITH_EDITOR
+
+void UDMLAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (AActor* OwningActor = GetOwningActor())
+	{
+		if (UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwningActor))
+		{
+			InitializeWithAbilitySystem(ASC);
+		}
+	}
+}
+
---
+++ Source/DeadMatchLock/Public/DMLAnimInstance.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "Animation/AnimInstance.h"
+#include "DMLAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEADMATCHLOCK_API UDMLAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	UDMLAnimInstance(const FObjectInitializer& ObjectInitializer);
+
+	virtual void InitializeWithAbilitySystem(UAbilitySystemComponent* ASC);
+
+protected:
+
+#if WITH_EDITOR
+	virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
+#endif // WITH_EDITOR
+
+	virtual void NativeInitializeAnimation() override;
+
+protected:
+
+	// Gameplay tags that can be mapped to blueprint variables. The variables will automatically update as the tags are added or removed.
+	// These should be used instead of manually querying for the gameplay tags.
+	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
+};
